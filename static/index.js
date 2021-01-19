22f310077f7904f6209f03a8b09a0e146f5a819b
Haroune Mohammedi <hm@big-mama.io>
2021-01-19 08:57:19 +0100

    Feat: add datepicker

--- static/index.js
+++ static/index.js
@@ -7,9 +7,14 @@ const SALAWAT_NAMES = {
     "maghrib": "المغرب",
     "icha": "العشاء"
 };
+const DATE_FORMAT = "yyyy-mm-dd";
 
-async function fetchMawaqit(wilaya) {
-    let url = API_URL + "?wilayas=" + wilaya;
+async function fetchOneMawaqit(wilaya, date) {
+    let params = new URLSearchParams({
+        wilayas: wilaya,
+        days: date,
+    });
+    let url = API_URL + "?" + params.toString();
     // let options = {'mode': 'no-cors'};
 
     let response = await fetch(url)
@@ -29,12 +34,29 @@ function updateMawaqit(mawaqit, salawat = SALAWAT_NAMES) {
 
 async function refreshMawaqit() {
     let wilaya = $("#wilaya").val();
-    let mawaqit = await fetchMawaqit(wilaya);
+    let date = $("#datepicker").val();
+    let mawaqit = await fetchOneMawaqit(wilaya, date);
     updateMawaqit(mawaqit);
 }
 
+
+formatDate = (date) => date.toISOString().substring(0, 10);
+
 $('#wilaya').change(refreshMawaqit);
+$('#datepicker').change(refreshMawaqit);
 
 $(function () {
+    let today = formatDate(new Date());
+    $("#datepicker").datepicker({
+        uiLibrary: 'bootstrap4',
+        format: DATE_FORMAT,
+        value: today,
+        change: function(e) {
+            console.log('Change is fired');
+        },
+        select: function(e) {
+            console.log('Selected a new value');
+        }
+    });
     refreshMawaqit();
 });
