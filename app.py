e216ea4e7a0ef06196dfee41fc98fa3d4b6e4369
Haroune Mohammedi <hm@big-mama.io>
2021-01-23 20:12:39 +0100

    Feat: add flask-cors to allow access from other domains

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 
 from salat_dz.apiv1 import blueprint as apiv1
 from salat_dz.apiv1 import wilayas_values
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.add_url_rule('/', 'index', lambda : render_template('index.html', wilayas=wilayas_values))
     app.register_blueprint(apiv1)
     return app
